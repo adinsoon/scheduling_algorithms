73bb07c08dcda981774a7af91c8840496ebfa610
adinsoon <adinsoonS@gmail.com>
2021-02-03 18:47:37 +0100

    pages + calls generator
    Took 1 hour 16 minutes

--- generators.py
+++ generators.py
@@ -1,22 +1,46 @@
 from process import Process
+from frame import Frame
 from random import randrange
 
 
 def generate_processes(process_range, arrival_range, duration_range):
-    target_list = []
     """
-    :param process_range: how much process to generate
+    :param process_range: how many processes to generate
     :param arrival_range: specify the range of arrivals
     :param duration_range:  specify the range of durations (>0)
-    :param target_list: appends to the given list
     :return: list of processes
     """
+    target_list = []
+
     for i in range(process_range):
         process = Process(i, randrange(arrival_range),
                           randrange(1, duration_range))
         target_list.append(process)
+
     return target_list
 
 
-def generate_pages():
-    pass
+def generate_calls(calls_range, value_range):
+    """
+    :param calls_range: how many calls to generate
+    :param value_range: specify the range of calls value
+    """
+    calls_list: list = []
+
+    for i in range(calls_range):
+        calls_list.append(randrange(1, value_range + 1))
+
+    return calls_list
+
+
+def generate_frames(frames_range):
+    """
+    :param frames_range: how many frames to generate
+    :return: list of frames
+    """
+    target_list: list = []
+
+    for i in range(frames_range):
+        target_list.append(Frame(i))
+
+    return target_list
