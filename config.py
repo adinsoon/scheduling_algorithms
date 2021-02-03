2c93b7e3d609314d6804fa0aff2b061bca6365f3
adinsoon <adinsoonS@gmail.com>
2021-02-03 18:48:01 +0100

    config for FIFO and LRU
    Took 25 seconds

--- config.py
+++ config.py
@@ -3,27 +3,45 @@ config = {
     "SUB": {
         # use same params for RR and FCFS
         "USE_RR_TO_FCFS": 1,
-        # "USE_FIFO_TO_LRU": 0,
+        # use same params for FIFO and LRU
+        "USE_FIFO_TO_LRU": 1,
     },
     # ROUND-ROBIN
     "RR": {
         # how many processes to generate
-        "RR_PROCESS_RANGE": 35,
+        "PROCESS_RANGE": 35,
         # specify the range of arrivals
-        "RR_ARRIVAL_RANGE": 5,
+        "ARRIVAL_RANGE": 5,
         # specify the range of durations (>0)
-        "RR_DURATION_RANGE": 25,
+        "DURATION_RANGE": 25,
         # appends to the given list
-        "RR_QUANTUM": 5,
+        "QUANTUM": 5,
     },
     # FCFS
     "FCFS": {
         # how many processes to generate
-        "FCFS_PROCESS_RANGE": 5,
+        "PROCESS_RANGE": 15,
         # specify the range of arrivals
-        "FCFS_ARRIVAL_RANGE": 5,
+        "ARRIVAL_RANGE": 5,
         # specify the range of durations (>0)
-        "FCFS_DURATION_RANGE": 25,
+        "DURATION_RANGE": 25,
+    },
+    # FIFO
+    "FIFO": {
+        # how many calls to generate
+        "CALLS_RANGE": 15,
+        # specify the range of calls value
+        "CALLS_VALUE_RANGE": 5,
+        # how many frames to generate
+        "FRAMES_RANGE": 8,
+    },
+    # LRU
+    "LRU": {
+        # how many calls to generate
+        "CALLS_RANGE": 15,
+        # specify the range of calls value
+        "CALLS_VALUE_RANGE": 5,
+        # how many frames to generate
+        "FRAMES_RANGE": 8,
     },
 }
-
